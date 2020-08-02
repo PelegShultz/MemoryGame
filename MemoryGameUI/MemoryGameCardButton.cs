56969a05b22458d264e1426aa8317ef797b8ebe3
‪Peleg Shultz‬‏ <69113381+PelegShultz@users.noreply.github.com>
2020-08-02 19:32:31 +0300

    Add files via upload

---
+++ MemoryGameUI/MemoryGameCardButton.cs
@@ -0,0 +1,104 @@
+﻿
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MemoryGameLogic;
+
+namespace MemoryGameUI
+{
+    internal class MemoryGameCardButtuon : Button
+    {
+        private readonly PictureBox r_ImageInCard;
+        private int m_RowNumber;
+        private int m_ColumnNumber;
+        private MemoryGameCard m_LogicMemoryGameCardReference;
+
+        internal MemoryGameCardButtuon(MemoryGameCard i_GameCard, int i_RownNumber, int i_ColumnNumber)
+        {
+            StringBuilder imageURL = new StringBuilder();
+            
+            m_RowNumber = i_RownNumber;
+            m_ColumnNumber = i_ColumnNumber;
+            m_LogicMemoryGameCardReference = i_GameCard;
+            FlatStyle = FlatStyle.Flat;
+            FlatAppearance.BorderSize = 1;
+            r_ImageInCard = new PictureBox();
+            r_ImageInCard.Top = this.Top + 3;
+            r_ImageInCard.Left = this.Left + 3;
+            r_ImageInCard.Size = new Size(44, 44);
+            r_ImageInCard.SizeMode = PictureBoxSizeMode.StretchImage;
+            imageURL.Append(@"https://picsum.photos/id/");
+            imageURL.Append(m_LogicMemoryGameCardReference.CardValue);
+            imageURL.Append(@"/80");
+            r_ImageInCard.LoadAsync(imageURL.ToString());
+            r_ImageInCard.Visible = false;
+            this.Controls.Add(r_ImageInCard);
+        }
+
+        internal void UpdateImageURL()
+        {
+            StringBuilder imageURL = new StringBuilder();
+            imageURL.Append(@"https://picsum.photos/id/");
+            imageURL.Append(m_LogicMemoryGameCardReference.CardValue);
+            imageURL.Append(@"/80");
+            r_ImageInCard.LoadAsync(imageURL.ToString());
+        }
+
+        internal MemoryGameCard LogicMemoryGameCardReference
+        {
+            get
+            {
+                return m_LogicMemoryGameCardReference;
+            }
+
+            set
+            {
+                m_LogicMemoryGameCardReference = value;
+            }
+        }
+
+        internal int RowNumber
+        {
+            get
+            {
+                return m_RowNumber;
+            }
+
+            set
+            {
+                m_RowNumber = value;
+            }
+        }
+
+        internal int ColumnNumber
+        {
+            get
+            {
+                return m_ColumnNumber;
+            }
+
+            set
+            {
+                m_ColumnNumber = value;
+            }
+        }
+
+        public PictureBox ImageInCard
+        {
+            get
+            {
+                return r_ImageInCard;
+            }
+        }
+
+        internal void HideImage() 
+        {
+            r_ImageInCard.Visible = false;
+        }
+
+        internal void ShowImage() 
+        {
+            r_ImageInCard.Visible = true;
+        }
+    }
+}
